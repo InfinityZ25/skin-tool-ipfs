4f2b56e94847f66a558a48a0b75f51bb17eadaff
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-11-09 06:23:28 -0500

    Added endpoint to interact with st-python

--- src/main/java/us/jcedeno/skin/SpringBootSkinToolApplication.java
+++ src/main/java/us/jcedeno/skin/SpringBootSkinToolApplication.java
@@ -2,15 +2,24 @@ package us.jcedeno.skin;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import java.util.UUID;
 
 import org.mineskin.MineskinClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.ipfs.api.IPFS;
@@ -25,11 +34,19 @@ public class SpringBootSkinToolApplication {
 	private static @Getter MineskinClient mineskinClient;
 	private static @Getter IPFS ipfs;
 
+	private static @Getter String skinToolPythonEndpoint;
+
+	private static HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(15)).build();
+
 	public static void main(String[] args) {
 		// Get variables from environment
 		final var mineskinClientKey = getEnvOrEmpty("MINESKIN_KEY");
 		final var mineskinAgent = getEnvOrEmpty("MINESKIN_USR_AGENT");
 		final var ipfsAddress = getEnvOrEmpty("IPFS_HOST");
+		final var skinToolPythonUri = getEnvOrEmpty("SKIN_TOOL_PYTHON_URI");
+
+		// Set the SkinTool Python endpoint
+		skinToolPythonEndpoint = skinToolPythonUri.isEmpty() ? "http://localhost:8069" : skinToolPythonUri;
 
 		// Intialize IFPS client
 		ipfs = new IPFS(new MultiAddress(ipfsAddress.isEmpty() ? "/ip4/127.0.0.1/tcp/5001" : ipfsAddress));
@@ -46,6 +63,24 @@ public class SpringBootSkinToolApplication {
 	@GetMapping("/helloworld")
 	public String helloWorld() {
 		return "Hello World (" + System.currentTimeMillis() + ")!";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/generate/{id}", method = RequestMethod.GET)
+	public String generateSkins(@PathVariable("id") String id) {
+
+		// Make a get request
+
+		var request = HttpRequest.newBuilder(URI.create(skinToolPythonEndpoint + "/" + id))
+				.header("accept", "application/json").build();
+
+		try {
+			return client.send(request, BodyHandlers.ofString()).body();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return "";
 	}
 
 	@GetMapping("/get")
